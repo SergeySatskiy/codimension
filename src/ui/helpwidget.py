700a0d7cb24eddb62b597e5a92c41a5e00fa8f31
Sergey Satskiy <sergey.satskiy@gmail.com>
2011-10-21 14:53:23 +0000

    Adding support for Alt+Up/Alt+Down

--- src/ui/helpwidget.py
+++ src/ui/helpwidget.py
@@ -72,10 +72,10 @@ class QuickHelpWidget( HTMLTabWidget ):
           <td>Ctrl+Up/Down</td>
           <td>Scrolling without changing cursor position</td></tr>
         <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td></tr>
+          <td>Alt+Up/Down</td>
+          <td>Move cursor one paragraph up/down</td>
+          <td>Alt+Home/End</td>
+          <td>Move cursor to the beginning/end of the visible line</td></tr>
         <tr>
           <td>Alt+PgDown</td>
           <td>Back in history</td>
