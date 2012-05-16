445e5eb02d2e0aaf10cf4738db58b00382b24add
sergey.satskiy@gmail.com <sergey.satskiy@gmail.com@9ac966cd-8e55-ee93-ef0a-8c5e4ef0ab7a>
2012-05-16 03:06:38 +0000

    Better name for the future python graphics editor

--- trunk/src/ui/mainwindowtabwidgetbase.py
+++ trunk/src/ui/mainwindowtabwidgetbase.py
@@ -29,12 +29,12 @@ import uuid
 class MainWindowTabWidgetBase():
     " Base class for all the main window tab widgets "
 
-    Unknown          = -1
-    PlainTextEditor  = 0    # Text only editor (including python)
-    PythonEditor     = 1    # Text/graphics python only editor widget
-    PictureViewer    = 2
-    HTMLViewer       = 3
-    GeneratedDiagram = 4
+    Unknown              = -1
+    PlainTextEditor      = 0    # Text only editor (including python)
+    PythonGraphicsEditor = 1    # Text/graphics python only editor widget
+    PictureViewer        = 2
+    HTMLViewer           = 3
+    GeneratedDiagram     = 4
 
     def __init__( self ):
         self.__uuid = uuid.uuid1()
