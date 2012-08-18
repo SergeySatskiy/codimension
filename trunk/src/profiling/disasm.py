33612317452b49a805397c7c3d791cfb694a95ca
sergey.satskiy@gmail.com <sergey.satskiy@gmail.com@9ac966cd-8e55-ee93-ef0a-8c5e4ef0ab7a>
2012-08-18 05:16:15 +0000

    Initial revision of the disassembling feature

--- trunk/src/profiling/disasm.py
+++ trunk/src/profiling/disasm.py
@@ -25,6 +25,86 @@
 import os.path
 import sys
 from utils.globals import GlobalData
+from utils.compatibility import relpath
+from subprocess import Popen, PIPE
+
+
+
+def _getCode( workingDir, moduleToImport, name, importDirs ):
+    """ Cmd line to get disassemble """
+
+    if moduleToImport.endswith( ".py" ):
+        moduleToImport = moduleToImport[ : -3 ]
+    elif moduleToImport.endswith( ".py3" ):
+        moduleToImport = moduleToImport[ : -4 ]
+
+    srcCode = ""
+    if len( importDirs ) > 0:
+        srcCode += "import sys;"
+        for importPath in importDirs:
+            srcCode += "sys.path.append( '" + importPath + "' );"
+
+    srcCode += "import dis;" \
+               "import " + moduleToImport + " as m;" \
+               "dis.dis(" + "m." + name + ")"
+
+    return "cd " + workingDir + ";" + \
+           sys.executable + " -c '" + srcCode + "'"
+
+
+def _getCodeForStandaloneScript( path, name, importDirs ):
+    """ Provides a complete command line for two cases:
+        - there were no project loaded
+        - file does not belong to the project """
+
+    workingDir = os.path.dirname( path )
+    moduleToImport = os.path.basename( path )
+    return _getCode( workingDir, moduleToImport, name, importDirs )
+
+
+def _getCodeForNestedScript( projectTopDir, relativePath,
+                             name, importDirs ):
+    """ Provides a complete command line for a module which is in
+        a nested directory starting from a project top dir.
+        projectTopDir is like /a/b/c
+        relativePath is like d/e/f.py """
+    workingDir = projectTopDir
+    moduleToImport = relativePath.replace( os.path.sep, '.' )
+    return _getCode( workingDir, moduleToImport, name, importDirs )
+
+
+def _checkInitPresence( startDir, dirs ):
+    """ Checks that all pathes down have __init__ files.
+        Returns True if so
+        startDir is like /a/b/c
+        dirs is like d/e/f """
+    candidatePath = startDir + os.path.sep
+    parts = dirs.split( os.path.sep )
+    for part in parts:
+        candidatePath += part + os.path.sep
+        if os.path.exists( candidatePath + "__init__.py" ):
+            continue
+        if os.path.exists( candidatePath + "__init__.py3" ):
+            continue
+        return False
+    return True
+
+
+def runWithShell( commandLine ):
+    " Runs something via a shell "
+    process = Popen( commandLine, shell = True,
+                     stdin = PIPE, stdout = PIPE, stderr = PIPE )
+    process.stdin.close()
+    processStdout = process.stdout.read()
+    process.stdout.close()
+    processStderr = process.stderr.read()
+    process.stderr.close()
+    process.wait()
+
+    if process.returncode != 0:
+        raise Exception( "Error executing command: '" + \
+                         commandLine + "': " + processStderr )
+    return processStdout
 
 
 
@@ -38,25 +118,56 @@ def getDisassembled( path, name ):
     if not path.endswith( '.py' ) and not path.endswith( '.py3' ):
         raise Exception( "Path must point to a python file" )
 
+    cmdLine2 = ""
     project = GlobalData().project
-    if project.isLoaded():
-        pass
+    if project.isProjectFile:
+        # There are at least two options:
+        # - to import starting from the project top level dir
+        # - to import starting from the directory where the script is located
+        # Let's try both of them
+        projectTopDir = os.path.dirname( project.fileName )
+        scriptDir = os.path.dirname( path )
+        if projectTopDir == scriptDir:
+            cmdLine = _getCodeForStandaloneScript( path, name,
+                                                   project.importDirs )
+        else:
+            # Calculate the relative path
+            # it will be like 'c/d/e/my.py'
+            relativePath = relpath( path, projectTopDir )
+
+            # Make sure there are __init__ files in all paths down
+            if _checkInitPresence( projectTopDir,
+                                   os.path.dirname( relativePath ) ) == False:
+                # No init files in the structure, so there is no point to try
+                # importing from the project top directory
+                cmdLine = _getCodeForStandaloneScript( path,
+                                                       name,
+                                                       project.importDirs )
+            else:
+                # Need to try both options of importing
+                # We may get luck succesfully loading it
+                cmdLine = _getCodeForStandaloneScript( path,
+                                                       name,
+                                                       project.importDirs )
+
+                cmdLine2 = _getCodeForNestedScript( projectTopDir,
+                                                    relativePath,
+                                                    name,
+                                                    project.importDirs )
     else:
-        # No project, just cd to the dir
-        workingDir = os.path.dirname( path )
-        moduleToImport = os.path.basename( path )
-        if moduleToImport.endswith( ".py" ):
-            moduleToImport = moduleToImport[ : -3 ]
-        elif moduleToImport.endswith( ".py3" ):
-            moduleToImport = moduleToImport[ : -3 ]
+        cmdLine = _getCodeForStandaloneScript( path, name, project.importDirs )
 
-        srcCode = "import dis;" \
-                  "import " + moduleToImport + ";" \
-                  "dis.dis(" + name + ")"
-
-        commandLine = "cd " + workingDir + ";" + \
-                      sys.executable + " -c '" + srcCode + "'"
-
+    # cmdLine is formed in any case
+    # cmdLine2 only if certain conditions are met
+    try:
+        return runWithShell( cmdLine )
+    except:
+        if cmdLine2 != "":
+            try:
+                return runWithShell( cmdLine2 )
+            except:
+                pass
+    raise Exception( "Could not get '" + name + "' disassembled" )
 
 
 
