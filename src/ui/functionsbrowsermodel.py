1d202c91aa1feecf0d00cd9ee53e7a9d68de3a4e
Sergey Satskiy <sergey.satskiy@gmail.com>
2012-12-06 04:21:03 +0000

    Cosmetic changes

--- src/ui/functionsbrowsermodel.py
+++ src/ui/functionsbrowsermodel.py
@@ -33,10 +33,8 @@ import os.path
 from PyQt4.QtCore       import SIGNAL
 from PyQt4.QtCore       import QVariant
 from viewitems          import TreeViewFunctionItem
-from utils.globals      import GlobalData
 from utils.project      import CodimensionProject
 from browsermodelbase   import BrowserModelBase
-from utils.fileutils    import detectFileType, PythonFileType, Python3FileType
 
 
 class FunctionsBrowserModel( BrowserModelBase ):
@@ -83,7 +81,8 @@ class FunctionsBrowserModel( BrowserModelBase ):
         itemsToDelete = []
         for path in deletedPythonFiles:
             for item in self.rootItem.childItems:
-                if os.path.realpath( path ) == os.path.realpath( item.getPath() ):
+                if os.path.realpath( path ) == \
+                   os.path.realpath( item.getPath() ):
                     itemsToDelete.append( item )
 
         for item in itemsToDelete:
