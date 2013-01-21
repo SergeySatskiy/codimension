5e8992543ad4012cbb8f6437e6ae42e83ec17473
sergey.satskiy@gmail.com <sergey.satskiy@gmail.com@9ac966cd-8e55-ee93-ef0a-8c5e4ef0ab7a>
2013-01-21 03:52:09 +0000

    Created a tag for codimension version 1.8.

---
+++ tags/codimension/1.8/src/debugger/client/debugthread.py
@@ -0,0 +1,160 @@
+#
+# -*- coding: utf-8 -*-
+#
+# codimension - graphics python two-way code editor and analyzer
+# Copyright (C) 2010  Sergey Satskiy <sergey.satskiy@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id$
+#
+
+#
+# The file was taken from eric 4 and adopted for codimension.
+# Original copyright:
+# Copyright (c) 2002 - 2012 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+
+"""
+Module implementing the debug thread
+"""
+
+import bdb
+import sys
+
+from base import DebugBase
+
+
+
+class DebugThread( DebugBase ):
+    """
+    Class implementing a debug thread.
+
+    It represents a thread in the python interpreter that we are tracing.
+
+    Provides simple wrapper methods around bdb for the 'owning' client to
+    call to step etc.
+    """
+    def __init__( self, dbgClient, targ = None, args = None, kwargs = None,
+                  mainThread = 0 ):
+        """
+        Constructor
+
+        @param dbgClient the owning client
+        @param targ the target method in the run thread
+        @param args  arguments to be passed to the thread
+        @param kwargs arguments to be passed to the thread
+        @param mainThread 0 if this thread is not the mainscripts thread
+        """
+        DebugBase.__init__( self, dbgClient )
+
+        self._target = targ
+        self._args = args
+        self._kwargs = kwargs
+        self._mainThread = mainThread
+
+        # thread running tracks execution state of client code
+        # it will always be 0 for main thread as that is tracked
+        # by DebugClientThreads and Bdb...
+        self._threadRunning = 0
+
+        self.__ident = None     # id of this thread
+        self.__name = ""
+        return
+
+    def set_ident( self, threadID ):
+        """
+        Public method to set the id for this thread.
+
+        @param id id for this thread (int)
+        """
+        self.__ident = threadID
+        return
+
+    def get_ident( self ):
+        """
+        Public method to return the id of this thread.
+
+        @return the id of this thread (int)
+        """
+        return self.__ident
+
+    def get_name( self ):
+        """
+        Public method to return the name of this thread.
+
+        @return name of this thread (string)
+        """
+        return self.__name
+
+    def traceThread( self ):
+        """
+        Private method to setup tracing for this thread.
+        """
+        self.set_trace()
+        if not self._mainThread:
+            self.set_continue( 0 )
+        return
+
+    def bootstrap( self ):
+        """
+        Private method to bootstrap the thread.
+
+        It wraps the call to the user function to enable tracing
+        before hand.
+        """
+        try:
+            try:
+                self._threadRunning = 1
+                self.traceThread()
+                self._target( *self._args, **self._kwargs )
+            except bdb.BdbQuit:
+                pass
+        finally:
+            self._threadRunning = 0
+            self.quitting = 1
+            self._dbgClient.threadTerminated( self )
+            sys.settrace( None )
+            sys.setprofile( None )
+        return
+
+    def trace_dispatch( self, frame, event, arg ):
+        """
+        Private method wrapping the trace_dispatch of bdb.py.
+
+        It wraps the call to dispatch tracing into
+        bdb to make sure we have locked the client to prevent multiple
+        threads from entering the client event loop.
+
+        @param frame The current stack frame.
+        @param event The trace event (string)
+        @param arg The arguments
+        @return local trace function
+        """
+        try:
+            self._dbgClient.lockClient()
+            # if this thread came out of a lock, and we are quitting
+            # and we are still running, then get rid of
+            # tracing for this thread
+            if self.quitting and self._threadRunning:
+                sys.settrace( None )
+                sys.setprofile( None )
+            import threading
+            self.__name = threading.currentThread().getName()
+            retval = DebugBase.trace_dispatch( self, frame, event, arg )
+        finally:
+            self._dbgClient.unlockClient()
+
+        return retval
---
+++ tags/codimension/1.8/src/editor/lexer/lexervhdl.py
@@ -0,0 +1,70 @@
+#
+# -*- coding: utf-8 -*-
+#
+# codimension - graphics python two-way code editor and analyzer
+# Copyright (C) 2010  Sergey Satskiy <sergey.satskiy@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id$
+#
+
+#
+# The file was taken from eric 4.4.3 and adopted for codimension.
+# Original copyright:
+# Copyright (c) 2007 - 2010 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+
+
+""" VHDL lexer implementation """
+
+from PyQt4.Qsci     import QsciLexerVHDL
+from PyQt4.QtCore   import QString
+from lexer          import Lexer
+
+
+class LexerVHDL( QsciLexerVHDL, Lexer ):
+    """ Adds lexer dependant methods to the QScintilla's version """
+
+    def __init__( self, parent = None ):
+
+        QsciLexerVHDL.__init__( self, parent )
+        Lexer.__init__( self )
+
+        self.commentString = QString( "--" )
+        return
+
+    def initProperties( self ):
+        """ Initializes the lexer properties """
+
+        self.setFoldComments( 1 )
+        self.setFoldAtElse( 1 )
+        self.setFoldAtBegin( 1 )
+        self.setFoldAtParenthesis( 1 )
+        self.setFoldCompact( 1 )
+        return
+
+    def isCommentStyle( self, style ):
+        """ Checks if a style is a comment one """
+
+        return style in [ QsciLexerVHDL.Comment,
+                          QsciLexerVHDL.CommentLine ]
+
+    def isStringStyle( self, style ):
+        """ Checks if a style is a string one """
+
+        return style in [ QsciLexerVHDL.String,
+                          QsciLexerVHDL.UnclosedString ]
+
