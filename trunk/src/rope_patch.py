c0ecdca7860b044a2974d08f73d5f48327d48a99
sergey.satskiy@gmail.com <sergey.satskiy@gmail.com@9ac966cd-8e55-ee93-ef0a-8c5e4ef0ab7a>
2013-03-07 00:39:50 +0000

    New rope patch from spyderlib

--- trunk/src/rope_patch.py
+++ trunk/src/rope_patch.py
@@ -4,66 +4,48 @@
 # Licensed under the terms of the MIT License
 # (see spyderlib/__init__.py for details)
 
+# The patch is taken from spyderlib and adopted for codimension
+
 """
-Patching rope for better performances
-See this thread:
+Patching rope:
+
+[1] is not applicable for codimension
+
+[2] For better performances, see this thread:
 http://groups.google.com/group/rope-dev/browse_thread/thread/57de5731f202537a
+
+[3] To avoid considering folders without __init__.py as Python packages, thus
+avoiding side effects as non-working introspection features on a Python module
+or package when a folder in current directory has the same name.
+See this thread:
+http://groups.google.com/group/rope-dev/browse_thread/thread/924c4b5a6268e618
+
+[4] To avoid rope adding a 2 spaces indent to every docstring it gets, because
+it breaks the work of Sphinx on the Object Inspector.
 """
 
 import inspect
-
-
-def getargfromdoc(obj):
-    """Get arguments from object doc"""
-    doc, name = obj.__doc__, obj.__name__
-    if doc is not None and name+'(' in doc:
-        return doc[doc.find(name+'(')+len(name)+1:doc.find(')')].split()
-
-def getargs(obj):
-    """Get the names and default values of a function's arguments"""
-    if inspect.isfunction(obj) or inspect.isbuiltin(obj):
-        func_obj = obj
-    elif inspect.ismethod(obj):
-        func_obj = obj.im_func
-    elif inspect.isclass(obj) and hasattr(obj, '__init__'):
-        func_obj = getattr(obj, '__init__')
-    else:
-        return []
-    if not hasattr(func_obj, 'func_code'):
-        # Builtin: try to extract info from doc
-        return getargfromdoc(func_obj)
-    args, _, _ = inspect.getargs(func_obj.func_code)
-    if not args:
-        return getargfromdoc(obj)
-
-    # Supporting tuple arguments in def statement:
-    for i_arg, arg in enumerate(args):
-        if isinstance(arg, list):
-            args[i_arg] = "(%s)" % ", ".join(arg)
-
-    defaults = func_obj.func_defaults
-    if defaults is not None:
-        for index, default in enumerate(defaults):
-            args[index+len(args)-len(defaults)] += '='+repr(default)
-    if inspect.isclass(obj) or inspect.ismethod(obj):
-        if len(args) == 1:
-            return None
-        if 'self' in args:
-            args.remove('self')
-    return args
+import rope
+from rope.base import pycore
+from rope.base import builtins, libutils, pyobjects
+import os.path as osp
+from rope.contrib import codeassist
+from spyderlib.utils.dochelpers import getdoc
+from rope.base import exceptions
 
 
 def applyRopePatch():
-    """Monkey patching rope for better performances"""
-    from rope import VERSION as ROPE_VERSION
-    if ROPE_VERSION not in ('0.9.4', '0.9.3', '0.9.2'):
-        raise ImportError, "rope %s can't be patched" % ROPE_VERSION
+    """Monkey patching rope for better performance"""
+    if rope.VERSION not in ('0.9.4', '0.9.3', '0.9.2'):
+        raise ImportError, "rope %s can't be patched" % rope.VERSION
 
-    # Patching pycore.PyCore, so that forced builtin modules (i.e. modules
-    # that were declared as 'extension_modules' in rope preferences)
-    # will be indeed recognized as builtins by rope, as expected
-    from rope.base import pycore
+    # Patching pycore.PyCore...
     class PatchedPyCore(pycore.PyCore):
+        # [2] ...so that forced builtin modules (i.e. modules that were
+        # declared as 'extension_modules' in rope preferences) will be indeed
+        # recognized as builtins by rope, as expected
+        #
+        # This patch is included in rope 0.9.4+ but applying it anyway is ok
         def get_module(self, name, folder=None):
             """Returns a `PyObject` if the module was found."""
             # check if this is a builtin module
@@ -75,28 +57,28 @@ def applyRopePatch():
                 raise pycore.ModuleNotFoundError(
                                             'Module %s not found' % name)
             return self.resource_to_pyobject(module)
+        # [3] ...to avoid considering folders without __init__.py as Python
+        # packages
+        def _find_module_in_folder(self, folder, modname):
+            module = folder
+            packages = modname.split('.')
+            for pkg in packages[:-1]:
+                if  module.is_folder() and module.has_child(pkg):
+                    module = module.get_child(pkg)
+                else:
+                    return None
+            if module.is_folder():
+                if module.has_child(packages[-1]) and \
+                   module.get_child(packages[-1]).is_folder() and \
+                   module.get_child(packages[-1]).has_child('__init__.py'):
+                    return module.get_child(packages[-1])
+                elif module.has_child(packages[-1] + '.py') and \
+                     not module.get_child(packages[-1] + '.py').is_folder():
+                    return module.get_child(packages[-1] + '.py')
     pycore.PyCore = PatchedPyCore
 
-    # Patching BuiltinFunction for the calltip/doc functions to be
-    # able to retrieve the function signatures with forced builtins
-    from rope.base import builtins, pyobjects
-    class PatchedBuiltinFunction(builtins.BuiltinFunction):
-        def __init__(self, returned=None, function=None, builtin=None,
-                     argnames=[], parent=None):
-            builtins._BuiltinElement.__init__(self, builtin, parent)
-            pyobjects.AbstractFunction.__init__(self)
-            self.argnames = argnames
-            if not argnames and builtin:
-                self.argnames = getargs(self.builtin)
-            if self.argnames is None:
-                self.argnames = []
-            self.returned = returned
-            self.function = function
-    builtins.BuiltinFunction = PatchedBuiltinFunction
-
-    # Patching BuiltinName for the go to definition feature to simply work
+    # [2] Patching BuiltinName for the go to definition feature to simply work
     # with forced builtins
-    from rope.base import libutils
     class PatchedBuiltinName(builtins.BuiltinName):
         def _pycore(self):
             p = self.pyobject
@@ -108,6 +90,8 @@ def applyRopePatch():
             if not inspect.isbuiltin(self.pyobject):
                 _lines, lineno = inspect.getsourcelines(self.pyobject.builtin)
                 path = inspect.getfile(self.pyobject.builtin)
+                if path.endswith('pyc') and osp.isfile(path[:-1]):
+                    path = path[:-1]
                 pycore = self._pycore()
                 if pycore and pycore.project:
                     resource = libutils.path_to_resource(pycore.project, path)
@@ -115,4 +99,88 @@ def applyRopePatch():
                     return (module, lineno)
             return (None, None)
     builtins.BuiltinName = PatchedBuiltinName
+
+    # [4] Patching several PyDocExtractor methods:
+    # 1. get_doc:
+    # To force rope to return the docstring of any object which has one, even
+    # if it's not an instance of AbstractFunction, AbstractClass, or
+    # AbstractModule.
+    # Also, to use utils.dochelpers.getdoc to get docs from forced builtins.
+    #
+    # 2. _get_class_docstring and _get_single_function_docstring:
+    # To not let rope add a 2 spaces indentation to every docstring, which was
+    # breaking our rich text mode. The only value that we are modifying is the
+    # 'indents' keyword of those methods, from 2 to 0.
+    #
+    # 3. get_calltip
+    # To easily get calltips of forced builtins
+    class PatchedPyDocExtractor(codeassist.PyDocExtractor):
+        def get_builtin_doc(self, pyobject):
+            buitin = pyobject.builtin
+            return getdoc(buitin)
+
+        def get_doc(self, pyobject):
+            if hasattr(pyobject, 'builtin'):
+                doc = self.get_builtin_doc(pyobject)
+                return doc
+            elif isinstance(pyobject, builtins.BuiltinModule):
+                docstring = pyobject.get_doc()
+                if docstring is not None:
+                    docstring = self._trim_docstring(docstring)
+                else:
+                    docstring = ''
+                # TODO: Add a module_name key, so that the name could appear
+                # on the OI text filed but not be used by sphinx to render
+                # the page
+                doc = {'name': '',
+                       'argspec': '',
+                       'note': '',
+                       'docstring': docstring
+                       }
+                return doc
+            elif isinstance(pyobject, pyobjects.AbstractFunction):
+                return self._get_function_docstring(pyobject)
+            elif isinstance(pyobject, pyobjects.AbstractClass):
+                return self._get_class_docstring(pyobject)
+            elif isinstance(pyobject, pyobjects.AbstractModule):
+                return self._trim_docstring(pyobject.get_doc())
+            elif pyobject.get_doc() is not None:  # Spyder patch
+                return self._trim_docstring(pyobject.get_doc())
+            return None
+
+        def get_calltip(self, pyobject, ignore_unknown=False, remove_self=False):
+            if hasattr(pyobject, 'builtin'):
+                doc = self.get_builtin_doc(pyobject)
+                return doc['name'] + doc['argspec']
+            try:
+                if isinstance(pyobject, pyobjects.AbstractClass):
+                    pyobject = pyobject['__init__'].get_object()
+                if not isinstance(pyobject, pyobjects.AbstractFunction):
+                    pyobject = pyobject['__call__'].get_object()
+            except exceptions.AttributeNotFoundError:
+                return None
+            if ignore_unknown and not isinstance(pyobject, pyobjects.PyFunction):
+                return None
+            if isinstance(pyobject, pyobjects.AbstractFunction):
+                result = self._get_function_signature(pyobject, add_module=True)
+                if remove_self and self._is_method(pyobject):
+                    return result.replace('(self)', '()').replace('(self, ', '(')
+                return result
+
+        def _get_class_docstring(self, pyclass):
+            contents = self._trim_docstring(pyclass.get_doc(), indents=0)
+            supers = [super.get_name() for super in pyclass.get_superclasses()]
+            doc = 'class %s(%s):\n\n' % (pyclass.get_name(), ', '.join(supers)) + contents
+
+            if '__init__' in pyclass:
+                init = pyclass['__init__'].get_object()
+                if isinstance(init, pyobjects.AbstractFunction):
+                    doc += '\n\n' + self._get_single_function_docstring(init)
+            return doc
+
+        def _get_single_function_docstring(self, pyfunction):
+            # signature = self._get_function_signature(pyfunction)
+            docs = pyfunction.get_doc()
+            return self._trim_docstring(docs, indents=0)
+    codeassist.PyDocExtractor = PatchedPyDocExtractor
 
