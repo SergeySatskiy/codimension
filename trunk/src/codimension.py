2b6384585474ea5ab365d2defa40a8473317f651
sergey.satskiy@gmail.com <sergey.satskiy@gmail.com@9ac966cd-8e55-ee93-ef0a-8c5e4ef0ab7a>
2013-07-03 00:25:52 +0000

    Updated version format

--- trunk/src/codimension.py
+++ trunk/src/codimension.py
@@ -25,7 +25,7 @@ codimension main Python script.
 It performs necessery initialization and starts the Qt main loop.
 """
 
-__version__ = "0.0"
+__version__ = "0.0.0"
 
 import sys, os, os.path
 from PyQt4.QtCore import SIGNAL, SLOT, QTimer, QDir, QTextCodec
