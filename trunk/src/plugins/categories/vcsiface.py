2a73ca0e706f0ddaa34cacde0fd3b65e2ac63cc1
sergey.satskiy@gmail.com <sergey.satskiy@gmail.com@9ac966cd-8e55-ee93-ef0a-8c5e4ef0ab7a>
2013-07-22 18:39:48 +0000

    More on plugins support

--- trunk/src/plugins/categories/vcsiface.py
+++ trunk/src/plugins/categories/vcsiface.py
@@ -57,9 +57,15 @@ class VersionControlSystemInterface( IPlugin ):
         """ Should provide the specific version control name, e.g. SVN """
         raise Exception( "getVCSName() must be overridden" )
 
-    def activate( self ):
+    def activate( self, ideSettings, ideGlobalData ):
         """ The plugin may override the method to do specific
             plugin activation handling.
+
+            ideSettings - reference to the IDE Settings singleton
+                          see codimension/src/utils/settings.py
+            ideGlobalData - reference to the IDE global settings
+                            see codimension/src/utils/globals.py
+
             Note: if overriden do not forget to call the
                   base class activate() """
         IPlugin.activate( self )
@@ -73,29 +79,47 @@ class VersionControlSystemInterface( IPlugin ):
         IPlugin.deactivate( self )
         return
 
-    def setEnvironment( self, ideSettings, ideGlobalData ):
-        """ Codimension calls this method after the activate() call and
-            before any other non-static methods.
-
-            ideSettings - reference to the IDE Settings singleton
-                          see codimension/src/utils/settings.py
-            ideGlobalData - reference to the IDE global settings
-                            see codimension/src/utils/globals.py
-
-            No return value is expected
-        """
-        return
-
     def populateMainMenu( self, parentMenu ):
+        """ The main menu looks as follows:
+            Plugins
+                - Mange plugins (fixed item)
+                - Separator (fixed item)
+                - <Plugin #1 name> (this is the parentMenu passed)
+                ...
+            If no items were populated by the plugin then there will be no
+            <Plugin #N name> menu item shown.
+            It is suggested to insert plugin configuration item here if so.
+        """
         raise Exception( "populateMainMenu() must be overridden" )
 
     def populateFileContextMenu( self, parentMenu ):
+        """ The file context menu shown in the project viewer window will have
+            an item with a plugin name and subitems which are populated here.
+            If no items were populated then the plugin menu item will not be
+            shown.
+            When a callback is called the corresponding menu item will have
+            attached data with an absolute path to the item.
+        """
         raise Exception( "populateFileContextMenu() must be overridden" )
 
     def populateDirectoryContextMenu( self, parentMenu ):
+        """ The directory context menu shown in the project viewer window will have
+            an item with a plugin name and subitems which are populated here.
+            If no items were populated then the plugin menu item will not be
+            shown.
+            When a callback is called the corresponding menu item will have
+            attached data with an absolute path to the directory.
+        """
         raise Exception( "populateDirectoryContextMenu() must be overridden" )
 
     def populateBufferContextMenu( self, parentMenu ):
+        """ The buffer context menu shown for the current edited/viewed file
+            will have an item with a plugin name and subitems which are populated here.
+            If no items were populated then the plugin menu item will not be
+            shown.
+            When a callback is called the corresponding menu item will have
+            attached data with the buffer UUID.
+        """
         raise Exception( "populateBufferContextMenu() must be overridden" )
 
     def isUnderVCS( self, path ):
@@ -104,13 +128,44 @@ class VersionControlSystemInterface( IPlugin ):
             revision control system type, or False otherwise. """
         raise Exception( "isUnderVCS() must be overridden" )
 
-    def isChangedLocally( self, path, recursively = False ):
+    def isChangedLocally( self, path, recursive = False ):
+        """ 'path' is an absolute path to a directory or to a file.
+            If the path is a directory then the 'recursive' argument
+            could be set to True. If the path is a file then the
+            'recursive' argument should be ignored.
+
+            The expected return value is a list of tuples:
+            [ (relpath, bool), (relpath, bool)... ]
+            where relpath is a relative path to the item (empty string for a file)
+            and bool is True if the item changed locally
+        """
         raise Exception( "isChangedLocally() must be overridden" )
 
-    def isChangedRemotely( self, path, recursively = False ):
+    def isChangedRemotely( self, path, recursive = False ):
+        """ 'path' is an absolute path to a directory or to a file.
+            If the path is a directory then the 'recursive' argument
+            could be set to True. If the path is a file then the
+            'recursive' argument should be ignored.
+
+            The expected return value is a list of tuples:
+            [ (relpath, bool), (relpath, bool)... ]
+            where relpath is a relative path to the item (empty string for a file)
+            and bool is True if the item changed in the repository
+        """
         raise Exception( "isChangedRemotely() must be overridden" )
 
-    def getInfo( self, path, recursively = False ):
+    def getInfo( self, path, recursive = False ):
+        """ 'path' is an absolute path to a directory or to a file.
+            If the path is a directory then the 'recursive' argument
+            could be set to True. If the path is a file then the
+            'recursive' argument should be ignored.
+
+            The expected return value is a list of tuples:
+            [ (relpath, string), (relpath, string)... ]
+            where relpath is a relative path to the item (empty string for a file)
+            and string is the textual description of what the VCS can tell about
+            the item.
+        """
         raise Exception( "getInfo() must be overridden" )
 
     def getRepositoryVersion( self, path, revision = None ):
