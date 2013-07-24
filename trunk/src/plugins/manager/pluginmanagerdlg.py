08d6f314882f57bec2ed7719823d6b76cf97a529
sergey.satskiy@gmail.com <sergey.satskiy@gmail.com@9ac966cd-8e55-ee93-ef0a-8c5e4ef0ab7a>
2013-07-24 03:14:01 +0000

    Some bits for the layout

--- trunk/src/plugins/manager/pluginmanagerdlg.py
+++ trunk/src/plugins/manager/pluginmanagerdlg.py
@@ -22,10 +22,10 @@
 
 """ Plugins manager dialog """
 
-from PyQt4.QtCore import Qt, SIGNAL
+from PyQt4.QtCore import Qt, SIGNAL, QStringList
 from PyQt4.QtGui import ( QDialog, QTreeWidgetItem, QTreeWidget, QVBoxLayout,
-                          QTextEdit, QDialogButtonBox )
-from itemdelegates import NoOutlineHeightDelegate
+                          QTextEdit, QDialogButtonBox, QLabel, QFontMetrics )
+from ui.itemdelegates import NoOutlineHeightDelegate
 
 
 class PluginItem( QTreeWidgetItem ):
@@ -44,13 +44,15 @@ class PluginsDialog( QDialog ):
         self.__pluginManager = pluginManager
         self.__createLayout()
         self.__populate()
+        self.__pluginsView.setFocus()
         return
 
     def __createLayout( self ):
         " Creates the dialog layout "
+        self.resize( 640, 480 )
+        self.setSizeGripEnabled( True )
 
         layout = QVBoxLayout()
-        layout.setContentsMargins( 1, 1, 1, 1 )
 
         # Plugins list
         self.__pluginsView = QTreeWidget()
@@ -64,7 +66,7 @@ class PluginsDialog( QDialog ):
         self.__pluginsHeader = QTreeWidgetItem(
                 QStringList() << "Alert" << "System/user" << "Enable" << "Name" << "Version" )
         self.__pluginsView.setHeaderItem( self.__pluginsHeader )
-        self.__pluginsHeader.header().setSortIndicator( 2, Qt.AscendingOrder )
+        self.__pluginsView.header().setSortIndicator( 2, Qt.AscendingOrder )
 
         layout.addWidget( self.__pluginsView )
 
@@ -74,6 +76,9 @@ class PluginsDialog( QDialog ):
         self.__detailsText = QTextEdit()
         self.__detailsText.setReadOnly( True )
         self.__detailsText.setAcceptRichText( False )
+        metrics = QFontMetrics( self.__detailsText.font() )
+        rect = metrics.boundingRect( "X" )
+        self.__detailsText.setFixedHeight( rect.height() * 6 + 5 )
         layout.addWidget( self.__detailsText )
 
         # Errors/warnings
@@ -82,6 +87,9 @@ class PluginsDialog( QDialog ):
         self.__errorsText = QTextEdit()
         self.__errorsText.setReadOnly( True )
         self.__errorsText.setAcceptRichText( False )
+        metrics = QFontMetrics( self.__errorsText.font() )
+        rect = metrics.boundingRect( "X" )
+        self.__errorsText.setFixedHeight( rect.height() * 4 + 5 )
         layout.addWidget( self.__errorsText )
 
         # Buttons box
@@ -97,4 +105,7 @@ class PluginsDialog( QDialog ):
         self.setLayout( layout )
         return
 
+    def __populate( self ):
+        " Populates the list with the plugins "
+        return
 
