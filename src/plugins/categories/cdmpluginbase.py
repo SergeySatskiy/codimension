e750f8f2b697b4acdef868f5ffc4749cf502350c
Sergey Satskiy <sergey.satskiy@gmail.com>
2013-07-31 22:40:07 +0000

    More on plugin interface

--- src/plugins/categories/cdmpluginbase.py
+++ src/plugins/categories/cdmpluginbase.py
@@ -42,7 +42,8 @@ class CDMPluginBase( IPlugin, QObject ):
         return
 
     def activate( self, ideSettings, ideGlobalData ):
-        " Activates the plugin "
+        """ Activates the plugin and saves references to
+            the IDE settings and global data """
         IPlugin.activate( self )
 
         self.ideSettings = ideSettings
@@ -50,7 +51,8 @@ class CDMPluginBase( IPlugin, QObject ):
         return
 
     def deactivate( self ):
-        " Deactivates the plugin "
+        """ Deactivates the plugin and clears references to
+            the IDE settings and global data """
         self.ideSettings = None
         self.ideGlobalData = None
 
@@ -67,25 +69,29 @@ class CDMPluginBase( IPlugin, QObject ):
         return None
 
     def getApplication( self ):
-        " Provides a reference to the codimension application "
+        """ Provides a reference to the codimension application.
+            See details in src/ui/application.py """
         if self.ideGlobalData is None:
             raise Exception( "Plugin is not active" )
         return self.ideGlobalData.application
 
     def getMainWindow( self ):
-        " Provides a reference to the application main window "
+        """ Provides a reference to the application main window.
+            See details in src/ui/mainwindow.py """
         if self.ideGlobalData is None:
             raise Exception( "Plugin is not active" )
         return self.ideGlobalData.mainWindow
 
     def getSkin( self ):
-        " Provides a reference to the current skin "
+        """ Provides a reference to the current skin.
+            See details in src/utils/skin.py """
         if self.ideGlobalData is None:
             raise Exception( "Plugin is not active" )
         return self.ideGlobalData.skin
 
     def getProject( self ):
         """ Provides a reference to the current project.
+            See details in src/utils/project.py
             Note: an object is provided even if there is no project loaded.
                   To check if a project is loaded use
                   getProject().isLoaded()
@@ -95,14 +101,18 @@ class CDMPluginBase( IPlugin, QObject ):
         return self.ideGlobalData.project
 
     def getIDESettingsDir( self ):
-        " Provides the directory where the IDE settings files are stored "
+        """ Provides the directory where the IDE setting files are stored.
+            The directory is individual for each user and it is usually
+            ~/.codimension/
+        """
         return settingsDir
 
     def getProjectSettingsDir( self ):
         """ Provides the directory where settings specific for the current
-            project are stored. If there is no project loaded then None is
-            returned.
-            The directory is individual for each user.
+            project are stored. If there is no project loaded at the time of
+            calling then None is returned.
+            The directory is individual for each user/project and it is usually
+            ~/.codimension/<project UUID>
         """
         project = self.getProject()
         if project.isLoaded():
@@ -114,17 +124,35 @@ class CDMPluginBase( IPlugin, QObject ):
         return self.getMainWindow().menuBar()
 
     def getStatusBar( self ):
-        " Provides a reference to the codimension main window status bar "
+        " Provides a reference to the codimension main window status bar (QStatusBar) "
         return self.getMainWindow().statusBar()
 
     def getMainToolbar( self ):
-        " Provides a reference to the main window toolbar "
-        pass
+        " Provides a reference to the main window toolbar (QToolBar) "
+        return self.getMainWindow().getToolbar()
 
     def getEditorsManager( self ):
-        pass
+        """ Provides a reference to the editors manager (it derives QTabWidget)
+            See details in src/ui/editorsmanager.py
+        """
+        return self.getMainWindow().editorsManagerWidget.editorsManager
 
     def getCurrentEditorWidget( self ):
-        pass
+        """ Provides a reference to the current widget.
+            Note: the widget could be of various types e.g. pixmap viewer, html viewer,
+                  text editor etc. All of them derive from MainWindowTabWidgetBase (see
+                  details in src/ui/mainwindowtabwidgetbase.py)
+                  The getCurrentEditorWidget().getType() call provides the current widget type.
+                  The known widget types are described in src/ui/mainwindowtabwidgetbase.py
+                  Depending on the widget type various functionality is avalable. See the
+                  certain widget implementation files:
+                  PlainTextEditor     src/editor/texteditor.py
+                  PictureViewer       src/ui/pixmapwidget.py
+                  HTMLViewer          src/ui/htmltabwidget.py
+                  GeneratedDiagram    src/diagram/importsdgmgraphics.py
+                  ProfileViewer       src/profiling/profwidget.py
+                  DisassemblerViewer  src/profiling/disasmwidget.py
+        """
+        return self.getEditorsManager().currentWidget()
 
 
