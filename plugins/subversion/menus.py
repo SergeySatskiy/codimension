db4feca969719d10c6d2236fa5d0a05bd662d2c2
Sergey Satskiy <sergey.satskiy@gmail.com>
2013-09-25 22:50:57 +0000

    Better handling of the context menus

--- plugins/subversion/menus.py
+++ plugins/subversion/menus.py
@@ -23,27 +23,83 @@
 " Menu provider for codimension subversion plugin "
 
 
+from PyQt4.QtCore import SIGNAL
+import os.path
+from svnindicators import IND_ERROR
+
+
+
 def populateMainMenu( plugin, parentMenu ):
     " Populates subversion plugin main menu "
+    plugin.connect( parentMenu, SIGNAL( "aboutToShow()" ),
+                    plugin.onMainMenuAboutToShow )
     parentMenu.addAction( "Configure", plugin.configure )
     return
 
 def populateFileContextMenu( plugin, parentMenu ):
     " Populates a context menu used for a file in a project browser "
     plugin.fileParentMenu = parentMenu
-    parentMenu.addAction( "Info", plugin.fileInfo )
+    plugin.connect( parentMenu, SIGNAL( "aboutToShow()" ),
+                    plugin.onFileContextMenuAboutToShow )
+    plugin.fileContextInfoAct = parentMenu.addAction( "&Info", plugin.fileInfo )
     return
 
 def populateDirectoryContextMenu( plugin, parentMenu ):
     " Populates a context menu used for a directory in a project browser "
     plugin.dirParentMenu = parentMenu
-    parentMenu.addAction( "Info", plugin.dirInfo )
+    plugin.connect( parentMenu, SIGNAL( "aboutToShow()" ),
+                    plugin.onDirectoryContextMenuAboutToShow )
+    plugin.dirContextInfoAct = parentMenu.addAction( "&Info", plugin.dirInfo )
     return
 
 def populateBufferContextMenu( plugin, parentMenu ):
     " Populates a context menu used for a text editor or a viewer "
-    parentMenu.addAction( "Configure", plugin.configure )
+    plugin.connect( parentMenu, SIGNAL( "aboutToShow()" ),
+                    plugin.onBufferContextMenuAboutToshow )
+    plugin.bufContextInfoAct = parentMenu.addAction( "&Info", plugin.bufferInfo )
     parentMenu.addSeparator()
-    parentMenu.addAction( "Info", plugin.bufferInfo )
+    parentMenu.addAction( "Configure", plugin.configure )
+    return
+
+
+
+def mainMenuAboutToShow( plugin ):
+    " Called when the plugin main menu is about to show "
+    return
+
+def fileContextMenuAboutToShow( plugin ):
+    " Called when the plugin file context menu is about to show "
+    path = str( plugin.fileParentMenu.menuAction().data().toString() )
+    if plugin.getLocalStatus( path ) in [ plugin.NOT_UNDER_VCS,
+                                          IND_ERROR ]:
+        plugin.fileContextInfoAct.setEnabled( False )
+        return
+
+    plugin.fileContextInfoAct.setEnabled( True )
+    return
+
+def directoryContextMenuAboutToShow( plugin ):
+    " Called when the plugin directory context manu is about to show "
+    path = str( plugin.dirParentMenu.menuAction().data().toString() )
+    if plugin.getLocalStatus( path ) in [ plugin.NOT_UNDER_VCS,
+                                          IND_ERROR ]:
+        plugin.dirContextInfoAct.setEnabled( False )
+        return
+
+    plugin.dirContextInfoAct.setEnabled( True )
+    return
+
+def bufferContextMenuAboutToshow( plugin ):
+    " Called when the plugin buffer context menu is about to show "
+    path = plugin.ide.currentEditorWidget.getFileName()
+    if not os.path.isabs( path ):
+        plugin.bufContextInfoAct.setEnabled( False )
+        return
+    if plugin.getLocalStatus( path ) in [ plugin.NOT_UNDER_VCS,
+                                          IND_ERROR ]:
+        plugin.bufContextInfoAct.setEnabled( False )
+        return
+
+    plugin.bufContextInfoAct.setEnabled( True )
     return
 
