b314011010143cfc824a7f4416d3ef0b3395c161
sergey.satskiy@gmail.com <sergey.satskiy@gmail.com@9ac966cd-8e55-ee93-ef0a-8c5e4ef0ab7a>
2013-10-16 20:19:51 +0000

    Better structuring of the SVN update functionality

--- trunk/plugins/subversion/svnupdate.py
+++ trunk/plugins/subversion/svnupdate.py
@@ -24,6 +24,7 @@
 
 import pysvn
 import logging
+import os.path
 from PyQt4.QtCore import Qt, SIGNAL, QTimer
 from PyQt4.QtGui import ( QDialog, QDialogButtonBox, QVBoxLayout, QLabel,
                           QApplication, QCursor )
@@ -31,17 +32,43 @@ from PyQt4.QtGui import ( QDialog, QDialogButtonBox, QVBoxLayout, QLabel,
 from svnstrconvert import notifyActionToString
 
 
+class SVNUpdateMixin:
 
-def doSVNUpdate( client, path, recursively, rev = None ):
-    " Performs the SVN Update for the given path "
+    def __init__( self ):
+        return
 
-    if rev is None:
-        rev = pysvn.Revision( pysvn.opt_revision_kind.head )
-    progressDialog = SVNUpdateProgress( client, path, recursively, rev )
-    QApplication.setOverrideCursor( QCursor( Qt.WaitCursor ) )
-    progressDialog.exec_()
-    QApplication.restoreOverrideCursor()
-    return
+    def fileUpdate( self ):
+        " Called when update for a file is requested "
+        path = str( self.fileParentMenu.menuAction().data().toString() )
+        self.__svnUpdate( path, False )
+        return
+
+    def dirUpdate( self ):
+        " Called when update for a directory is requested "
+        path = str( self.dirParentMenu.menuAction().data().toString() )
+        self.__svnUpdate( path, True )
+        return
+
+    def bufferUpdate( self ):
+        " Called when update for a buffer is requested "
+        path = self.ide.currentEditorWidget.getFileName()
+        if not os.path.isabs( path ):
+            logging.info( "SVN update is not applicable for never saved buffer" )
+            return
+        self.__svnUpdate( path, False )
+        return
+
+    def __svnUpdate( self, path, recursively, rev = None ):
+        " Does SVN update "
+        client = self.getSVNClient( self.getSettings() )
+        if rev is None:
+            rev = pysvn.Revision( pysvn.opt_revision_kind.head )
+        progressDialog = SVNUpdateProgress( client, path, recursively, rev )
+        QApplication.setOverrideCursor( QCursor( Qt.WaitCursor ) )
+        progressDialog.exec_()
+        QApplication.restoreOverrideCursor()
+        return
+
 
 
 class SVNUpdateProgress( QDialog ):
