27f983551c1de7b7d4d79b9a3c285275faadc620
Sergey Satskiy <sergey.satskiy@gmail.com>
2014-01-20 19:46:21 +0000

    Better SVN detailed info output

--- plugins/subversion/svninfo.py
+++ plugins/subversion/svninfo.py
@@ -97,27 +97,87 @@ class SVNInfoMixin:
         except:
             return None
 
-    def getServerRevision( self, client, info ):
+    def getServerInfoObject( self, client, info ):
         " Returns the server revision "
-        url = None
-        for item in info:
-            if item[ 0 ] == 'URL':
-                url = item[ 1 ]
-                break
-        if url is None:
-            return None
-
         repRevision = pysvn.Revision( pysvn.opt_revision_kind.head )
         try:
-            entries = client.info2( url, repRevision, recurse = False )
+            entries = client.info2( info.URL, repRevision, recurse = False )
             if len( entries ) != 1:
                 return None
             itemPath, info = entries[ 0 ]
-            if 'rev' in info and info[ 'rev' ]:
-                return info[ 'rev' ].number
+            return info
         except:
             return None
         return None
+
+    @staticmethod
+    def __getLockInfo( lockInfo ):
+        " Provides lock info as a list of strings "
+        info = []
+        if 'owner' in lockInfo:
+            if lockInfo[ 'owner' ]:
+                info.append( "Owner: " + lockInfo[ 'owner' ] )
+            else:
+                info.append( "Owner: Unknown" )
+        if 'creation_date' in lockInfo:
+            if lockInfo[ 'creation_date' ]:
+                info.append( "Creation date: " +
+                             timestampToString( lockInfo[ 'creation_date' ] ) )
+            else:
+                info.append( "Creation date: Unknown" )
+        if 'expiration_date' in lockInfo:
+            if lockInfo[ 'expiration_date' ]:
+                info.append( "Expiration date: " +
+                             timestampToString( lockInfo[ 'expiration_date' ] ) )
+            else:
+                info.append( "Expiration date: Unknown" )
+        if 'token' in lockInfo:
+            if lockInfo[ 'token' ]:
+                info.append( "Token: " + lockInfo[ 'token' ] )
+            else:
+                info.append( "Token: None" )
+        if 'comment' in lockInfo:
+            if lockInfo[ 'comment' ]:
+                info.append( "Comment: " +  lockInfo[ 'comment' ] )
+            else:
+                info.append( "Comment: None" )
+        return info
+
+    def __lastCommitMessage( self, client, serverInfoObject ):
+        " Provides the server last commit message "
+        revStart = pysvn.Revision( pysvn.opt_revision_kind.number,
+                                   serverInfoObject.last_changed_rev.number )
+        revEnd = pysvn.Revision( pysvn.opt_revision_kind.number,
+                                 serverInfoObject.last_changed_rev.number )
+
+        try:
+            return client.log( serverInfoObject.URL, revStart, revEnd,
+                               limit = 1 )[ 0 ].message
+        except Exception:
+            return "Could not get last change log message"
+
+    def __lastCommitProperties( self, client, serverInfoObject ):
+        " Provides the server properties "
+        rev = pysvn.Revision( pysvn.opt_revision_kind.number,
+                              serverInfoObject.last_changed_rev.number )
+        try:
+            propList = client.proplist( serverInfoObject.URL, rev,
+                                        recurse = False )
+            if len( propList ) != 1:
+                return None
+            return propList[ 0 ][ 1 ]
+        except:
+            return None
+
+    def __localProperties( self, client, path ):
+        " Provides the local copy properties "
+        try:
+            propList = client.proplist( path )
+            if len( propList ) != 1:
+                return None
+            return propList[ 0 ][ 1 ]
+        except:
+            return None
 
     def __svnInfo( self, path ):
         " Implementation of the info command for a file "
@@ -130,44 +190,79 @@ class SVNInfoMixin:
             return
 
         client = self.getSVNClient( self.getSettings() )
-        info = getSVNInfo( client, path )
-        statusObject = self.getLocalStatusObject( client, path )
-        serverRevisionNumber = self.getServerRevision( client, info )
-        serverStatusObject = self.getServerStatusObject( client, path )
-        message = "\n    Local content status: " + \
-                        rawStatusToString( statusObject.text_status ) + \
-                  "\n    Local properties status: " + \
-                        rawStatusToString( statusObject.prop_status )
 
-        if serverStatusObject is None:
-            message += "\n    Server status: could not get"
+        # Local info throws exceptions if something is broken
+        itemPath, localInfoObject = getSVNInfo( client, path )
+        serverInfoObject = self.getServerInfoObject( client, localInfoObject )
+        serverProperties = self.__lastCommitProperties( client, serverInfoObject )
+        localStatusObject = self.getLocalStatusObject( client, itemPath )
+        localProperties = self.__localProperties( client, itemPath )
+
+        message = "\nServer Info:"
+        if serverInfoObject is None:
+            message += "\n    Error getting server info"
         else:
-            if serverStatusObject.repos_text_status == pysvn.wc_status_kind.none:
-                serverContentStatus = "non modified"
+            message += "\n    Last commit:"
+            message += "\n        Revision: " + str( serverInfoObject.last_changed_rev.number )
+            message += "\n        Timestamp: " + timestampToString( serverInfoObject.last_changed_date )
+            message += "\n        Author: " + str( serverInfoObject.last_changed_author )
+            message += "\n        Message: " + str( self.__lastCommitMessage( client, serverInfoObject ) )
+            message += "\n    Properties:"
+            if serverProperties is None:
+                message += "\n        Error getting server properties"
             else:
-                serverContentStatus = rawStatusToString( serverStatusObject.repos_text_status )
-            if serverStatusObject.repos_prop_status == pysvn.wc_status_kind.none:
-                serverPropStatus = "non modified"
+                if len( serverProperties ) == 0:
+                    message += "\n        No properties"
+                else:
+                    for key in serverProperties.keys():
+                        message += "\n        " + key + ": " + serverProperties[ key ]
+            if serverInfoObject.lock is None:
+                message += "\n    Locked: False"
             else:
-                serverPropStatus = rawStatusToString( serverStatusObject.repos_prop_status )
+                message += "\n    Locked: True"
+                for line in self.__getLockInfo( serverInfoObject.lock ):
+                    message += "\n        " + line
+            message += "\n    Node:"
+            message += "\n        URL: " + serverInfoObject.URL
+            message += "\n        Repository root URL: " + serverInfoObject.repos_root_URL
+            message += "\n        Repository UUID: " + serverInfoObject.repos_UUID
+            message += "\n        Kind: " + nodeKindToString( serverInfoObject.kind )
 
-            message += "\n    Server content status: " + serverContentStatus + \
-                       "\n    Server properties status: " + serverPropStatus
-        if serverRevisionNumber is None:
-            message += "\n    Server revision: could not get"
+        message += "\nLocal Info:"
+        if localInfoObject is None:
+            message += "\n    Error getting local info"
         else:
-            message += "\n    Server revision: " + str( serverRevisionNumber )
+            message += "\n    Status:"
+            message += "\n        Content: " + rawStatusToString( localStatusObject.text_status )
+            message += "\n        Properties: " + rawStatusToString( localStatusObject.prop_status )
+            message += "\n    Checkout:"
+            message += "\n        Revision: " + str( localInfoObject.last_changed_rev.number )
+            message += "\n        Timestamp: " + timestampToString( localInfoObject.last_changed_date )
+            message += "\n        Author: " + str( localInfoObject.last_changed_author )
+            message += "\n    Properties:"
+            if localProperties is None:
+                message += "\n        Error getting local properties"
+            else:
+                if len( localProperties ) == 0:
+                    message += "\n        No properties"
+                else:
+                    for key in localProperties.keys():
+                        message += "\n        " + key + ": " + localProperties[ key ]
+            if localInfoObject.lock is None:
+                message += "\n    Locked: False"
+            else:
+                message += "\n    Locked: True"
+                for line in self.__getLockInfo( localInfoObject.lock ):
+                    message += "\n        " + line
+            message += "\n    Node:"
+            message += "\n        Path: " + itemPath
+            message += "\n        Copied: " + str( bool( localStatusObject.is_copied ) )
+            message += "\n        Switched: " + str( bool( localStatusObject.is_switched ) )
+            message += "\n        Schedule: " + scheduleToString( localInfoObject.wc_info.schedule )
 
-        message = message + "\n    Copied: " + str( bool( statusObject.is_copied ) ) + \
-                            "\n    Switched: " + str( bool( statusObject.is_switched ) ) + \
-                            "\n    Locked: " + str( bool( statusObject.is_locked ) )
-        for item in info:
-            message = message + "\n    " + item[ 0 ] + ": " + item[ 1 ]
         logging.info( message )
         client = None
         return
-
-
 
 def getSVNInfo( client, path, repRevision = None, pegRevision = None ):
     " Provides info for the given path "
@@ -176,7 +271,6 @@ def getSVNInfo( client, path, repRevision = None, pegRevision = None ):
     if pegRevision is None:
         pegRevision = pysvn.Revision( pysvn.opt_revision_kind.unspecified )
 
-    result = []
     try:
         entries = client.info2( path, repRevision, pegRevision,
                                 recurse = False )
@@ -184,86 +278,11 @@ def getSVNInfo( client, path, repRevision = None, pegRevision = None ):
             raise Exception( "Unexpected number of entries for the path. "
                              "Expected 1, received " + str( len( entries ) ) )
         itemPath, info = entries[ 0 ]
-        result.append( ("Path", itemPath) )
-        if 'URL' in info and info[ 'URL' ]:
-            result.append( ("URL", info[ 'URL' ]) )
-        if 'rev' in info and info[ 'rev' ]:
-            result.append( ("Local revision", str( info[ 'rev' ].number )) )
-        if 'repos_root_URL' in info and info[ 'repos_root_URL' ]:
-            result.append( ("Repository root URL", info[ 'repos_root_URL' ]) )
-        if 'repos_UUID' in info and info[ 'repos_UUID' ]:
-            result.append( ("Repository UUID", info[ 'repos_UUID' ]) )
-        if 'last_changed_author' in info and info[ 'last_changed_author' ]:
-            result.append( ("Author of the last changes",
-                            info[ 'last_changed_author' ]) )
-        if 'last_changed_date' in info and info[ 'last_changed_date' ]:
-            result.append( ("Date of the last changes",
-                            timestampToString( info[ 'last_changed_date' ] )) )
-        if 'last_changed_rev' in info and info[ 'last_changed_rev' ] and \
-           info[ 'last_changed_rev' ].kind == pysvn.opt_revision_kind.number:
-            result.append( ("Revision of the last changes",
-                            str( info[ 'last_changed_rev' ].number )) )
-        if 'kind' in info and info[ 'kind' ]:
-            result.append( ("Node kind", nodeKindToString( info[ 'kind' ] )) )
-        if 'lock' in info and info[ 'lock' ]:
-            lockInfo = info[ 'lock' ]
-            if 'owner' in lockInfo:
-                if lockInfo[ 'owner' ]:
-                    result.append( ("Lock owner", lockInfo[ 'owner' ]) )
-                else:
-                    result.append( ("Lock owner", "unknown") )
-            if 'creation_date' in lockInfo:
-                if lockInfo[ 'creation_date' ]:
-                    result.append( ("Lock creation date",
-                                    timestampToString( lockInfo[ 'creation_date' ] )) )
-                else:
-                    result.append( ("Lock creation date", "unknown") )
-            if 'expiration_date' in lockInfo:
-                if lockInfo[ 'expiration_date' ]:
-                    result.append( ("Lock expiration date",
-                                    timestampToString( lockInfo[ 'expiration_date' ] )) )
-                else:
-                    result.append( ("Lock expiration date", "unknown") )
-            if 'token' in lockInfo:
-                if lockInfo[ 'token' ]:
-                    result.append( ("Lock token", lockInfo[ 'token' ]) )
-                else:
-                    result.append( ("Lock token", "none") )
-            if 'comment' in lockInfo:
-                if lockInfo[ 'comment' ]:
-                    result.append( ("Lock comment", lockInfo[ 'comment' ]) )
-                else:
-                    result.append( ("Lock comment", "none") )
-        if 'wc_info' in info and info[ 'wc_info' ]:
-            wcInfo = info[ 'wc_info' ]
-            if 'schedule' in wcInfo and wcInfo[ 'schedule' ]:
-                result.append( ("Schedule",
-                                scheduleToString( wcInfo[ 'schedule' ] )) )
-            if 'copyfrom_url' in wcInfo and wcInfo[ 'copyfrom_url' ]:
-                result.append( ("Copied from URL", wcInfo[ 'copyfrom_url' ]) )
-            if 'copyfrom_rev' in wcInfo and wcInfo[ 'copyfrom_rev' ]:
-                if wcInfo[ 'copyfrom_rev' ].kind == pysvn.opt_revision_kind.number:
-                    if wcInfo[ 'copyfrom_rev' ].number != -1:
-                        result.append( ("Copied from revision",
-                                        str( wcInfo[ 'copyfrom_rev' ].number )) )
-            if 'text_time' in wcInfo and wcInfo[ 'text_time' ]:
-                result.append( ("Last time content updated",
-                                timestampToString( wcInfo[ 'text_time' ] )) )
-            if 'prop_time' in wcInfo and wcInfo[ 'prop_time' ]:
-                result.append( ("Last time properties updated",
-                                timestampToString( wcInfo[ 'prop_time' ] )) )
-            if 'checksum' in wcInfo and wcInfo[ 'checksum' ]:
-                result.append( ("Checksum", wcInfo[ 'checksum' ]) )
-        return result
-
+        return itemPath, info
     except pysvn.ClientError, exc:
         errorCode = exc.args[ 1 ][ 0 ][ 1 ]
         if errorCode in [ pysvn.svn_err.wc_not_working_copy,
                           pysvn.svn_err.wc_path_not_found ]:
-            return ( ("Status", "Not under SVN control"), )
+            raise Exception( "Not under SVN control" )
         message = exc.args[ 0 ]
-        return  ( ("Error", message), )
-    except Exception, exc:
-        return ( ("Error ", str( exc )), )
-    except:
-        return ( ("Error", "Unknown error"), )
+        raise Exception( message )
