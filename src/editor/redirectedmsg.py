7ff48a69651181e874411df50ae05c10e2b8a582
Sergey Satskiy <sergey.satskiy@gmail.com>
2014-04-22 15:49:40 +0000

    Implementation of the 'copy with timestamps' feature for the redirected IO console

--- src/editor/redirectedmsg.py
+++ src/editor/redirectedmsg.py
@@ -27,13 +27,17 @@ from utils.settings import Settings
 
 
 
-def getNowTimestamp():
-    " Provides the now() timestamp as a string "
-    timestamp = datetime.now()
-    millisecond = str( int( round( timestamp.microsecond / 1000.0 ) ) )
+def printableTimestamp( tstamp ):
+    " Provides a printable timestamp "
+    millisecond = str( int( round( tstamp.microsecond / 1000.0 ) ) )
     while len( millisecond ) != 3:
         millisecond = "0" + millisecond
-    return timestamp.strftime( "%H:%M:%S." ) + millisecond
+    return tstamp.strftime( "%H:%M:%S." ) + millisecond
+
+
+def getNowTimestamp():
+    " Provides the now() timestamp as a string "
+    return printableTimestamp( datetime.now() )
 
 
 
@@ -55,10 +59,7 @@ class IOConsoleMsg( object ):
 
     def getTimestamp( self ):
         " Provides the timestamp as a string "
-        millisecond = str( int( round( self.timestamp.microsecond / 1000.0 ) ) )
-        while len( millisecond ) != 3:
-            millisecond = "0" + millisecond
-        return self.timestamp.strftime( "%H:%M:%S." ) + millisecond
+        return printableTimestamp( self.timestamp )
 
 
 class IOConsoleMessages:
@@ -88,3 +89,58 @@ class IOConsoleMessages:
         self.msgs = []
         self.size = 0
         return
+
+    def renderWithTimestamps( self ):
+        " Provides the 'copy with timestamps' content "
+        buf = []
+        outputIndex = -1
+
+        streamPlaceholder = "   "
+        timestampPlaceholder = " " * 12
+        emptyPrefix = streamPlaceholder + " " + timestampPlaceholder
+
+        for msg in self.msgs:
+            if msg.msgType == IOConsoleMsg.IDE_MESSAGE:
+                txt = msg.msgText.strip()
+                parts = txt.splitlines()
+                if parts:
+                    buf.append( "ide " + msg.getTimestamp() + " " + parts[ 0 ] )
+                    for part in parts[ 1 : ]:
+                        buf.append( emptyPrefix + " " + part )
+                outputIndex = -1
+                continue
+
+            if msg.msgType not in [ IOConsoleMsg.STDOUT_MESSAGE,
+                                    IOConsoleMsg.STDERR_MESSAGE,
+                                    IOConsoleMsg.STDIN_MESSAGE ]:
+                raise Exception( "Unexpected message IO console type: " +
+                                 str( msg.msgType ) )
+
+            if msg.msgType == IOConsoleMsg.STDOUT_MESSAGE:
+                stream = "out"
+            elif msg.msgType == IOConsoleMsg.STDERR_MESSAGE:
+                stream = "err"
+            else:
+                stream = " in"
+
+            parts = msg.msgText.splitlines()
+            if parts:
+                if outputIndex == -1:
+                    buf.append( stream + " " + msg.getTimestamp() + " " + parts[ 0 ] )
+                    outputIndex = len( buf ) - 1
+                else:
+                    spaces = len( buf[ outputIndex ] ) - len( emptyPrefix ) - 1
+                    buf[ outputIndex ] += parts[ 0 ]
+                    buf.append( stream + " " + msg.getTimestamp() + " " + " " * spaces + "^" )
+
+                parts = parts[ 1 : ]
+                if parts:
+                    for part in parts:
+                        buf.append( stream + " " + msg.getTimestamp() + " " + part )
+                    outputIndex = len( buf ) - 1
+
+            if msg.msgText.endswith( '\n' ) or msg.msgText.endswith( '\r' ):
+                outputIndex = -1
+
+        return "\n".join( buf )
+
