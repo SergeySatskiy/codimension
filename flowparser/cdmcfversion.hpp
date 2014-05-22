12e8f98d8437e54c871e903618caacec9232cbe9
Sergey Satskiy <sergey.satskiy@gmail.com>
2014-05-22 04:15:32 +0000

    Initial version

---
+++ flowparser/cdmcfversion.hpp
@@ -0,0 +1,33 @@
+/*
+ * codimension - graphics python two-way code editor and analyzer
+ * Copyright (C) 2014  Sergey Satskiy <sergey.satskiy@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id: cflowmodule.hpp 2209 2014-05-20 04:21:47Z sergey.satskiy@gmail.com $
+ *
+ * Python extension module - versions
+ */
+
+#ifndef CDMCFVERSION_HPP
+#define CDMCFVERSION_HPP
+
+#define CMLVERSION  1
+
+#ifndef CDM_CF_PARSER_VERION
+#define CDM_CF_PARSER_VERION "trunk"
+#endif
+
+#endif
+
