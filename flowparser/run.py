65bcf084d51093297174dc382b55066cce4503b5
Sergey Satskiy <sergey.satskiy@gmail.com>
2015-03-20 11:25:43 -0400

    Adding a helper to run the parser

--- flowparser/run.py
+++ flowparser/run.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# $Id$
+# $Id: run.py 2397 2015-03-18 19:03:10Z sergey.satskiy@gmail.com $
 #
 
 " Convinience parser launcher "
