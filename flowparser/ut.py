66ea13976020afb7c93d00bff4002793896a8d34
Sergey Satskiy <sergey.satskiy@gmail.com>
2015-05-11 09:43:32 -0400

    More unit tests

--- flowparser/ut.py
+++ flowparser/ut.py
@@ -236,11 +236,11 @@ class CDMControlFlowParserTest( unittest.TestCase ):
                    "module docstring error - 2 lines, joined" )
         return
 
-#    def test_docstring7( self ):
-#        " Test docstring 7 "
-#        self.meat( self.dir + "docstring7.py",
-#                   'module docstring error - multilined with """' )
-#        return
+    def test_docstring7( self ):
+        " Test docstring 7 "
+        self.meat( self.dir + "docstring7.py",
+                   'module docstring error - multilined with """' )
+        return
 
 #    def test_function_definitions( self ):
 #        " Test function definitions"
@@ -337,11 +337,11 @@ class CDMControlFlowParserTest( unittest.TestCase ):
 #                   "one comment line test failed" )
 #        return
 
-#    def test_comments_only( self ):
-#        " Test for a file with no other lines except of comments "
+    def test_comments_only( self ):
+        " Test for a file with no other lines except of comments "
 
-#        self.meat( self.dir + "commentsonly.py",
-#                   "comments only with no other empty lines test failed" )
+        self.meat( self.dir + "commentsonly.py",
+                   "comments only with no other empty lines test failed" )
 
 #    def test_noendofline( self ):
 #        " Test for a file which has no EOL at the end "
