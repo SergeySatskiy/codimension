ce65f015b5c74c34620d7f0a6f9b0bf3961eda71
Sergey Satskiy <sergey.satskiy@gmail.com>
2015-08-12 16:05:36 -0400

    More on integration

--- src/editor/flowuiwidget.py
+++ src/editor/flowuiwidget.py
@@ -22,12 +22,19 @@
 
 " Control flow UI widget "
 
-from PyQt4.QtCore import Qt, QSize
+from PyQt4.QtCore import Qt, QSize, QTimer, SIGNAL
 from PyQt4.QtGui import ( QToolBar, QWidget, QGraphicsView, QPainter,
-                          QApplication, QGraphicsScene, QHBoxLayout )
+                          QApplication, QGraphicsScene, QHBoxLayout,
+                          QLabel )
 from cdmcf import getControlFlowFromMemory
 from flowui.vcanvas import VirtualCanvas
 from flowui.cflowsettings import getDefaultCflowSettings
+from utils.pixmapcache import PixmapCache
+from utils.globals import GlobalData
+from utils.fileutils import Python3FileType, PythonFileType
+
+
+IDLE_TIMEOUT = 1500
 
 
 class CFGraphicsView( QGraphicsView ):
@@ -65,10 +72,23 @@ class CFGraphicsView( QGraphicsView ):
 class FlowUIWidget( QWidget ):
     " The widget which goes along with the text editor "
 
+    STATE_OK_UTD = 0        # Parsed OK, control flow up to date
+    STATE_OK_CHN = 1        # Parsed OK, control flow changed
+    STATE_BROKEN_UTD = 2    # Parsed with errors, control flow up to date
+    STATE_BROKEN_CHN = 3    # Parsed with errors, control flow changed
+    STATE_UNKNOWN = 4
+
     def __init__( self, editor, parent ):
         QWidget.__init__( self, parent )
 
+        # It is always not visible at the beginning because there is no
+        # editor content at the start
+        self.setVisible( False )
+
         self.__editor = editor
+        self.__parentWidget = parent
+        self.__currentIconState = self.STATE_UNKNOWN
+        self.__connected = False
 
         self.cflowSettings = getDefaultCflowSettings( self )
 
@@ -76,22 +96,41 @@ class FlowUIWidget( QWidget ):
         hLayout.setContentsMargins( 0, 0, 0, 0 )
         hLayout.setSpacing( 0 )
 
+        # Make pylint happy
+        self.__toolbar = None
+        self.__infoIcon = None
+        self.__cf = None
+
+        # Create the update timer
+        self.__updateTimer = QTimer( self )
+        self.__updateTimer.setSingleShot( True )
+        self.__updateTimer.timeout.connect( self.process )
+
         hLayout.addWidget( self.__createGraphicsView() )
         hLayout.addWidget( self.__createToolbar() )
-
         self.setLayout( hLayout )
+
+        # Connect to the change file type signal
+        mainWindow = GlobalData().mainWindow
+        editorsManager = mainWindow.editorsManagerWidget.editorsManager
+        self.connect( editorsManager, SIGNAL( 'fileTypeChanged' ),
+                      self.__onFileTypeChanged )
         return
 
     def __createToolbar( self ):
         " Creates the toolbar "
-        toolbar = QToolBar( self )
-        toolbar.setOrientation( Qt.Vertical )
-        toolbar.setMovable( False )
-        toolbar.setAllowedAreas( Qt.RightToolBarArea )
-        toolbar.setIconSize( QSize( 16, 16 ) )
-        toolbar.setFixedWidth( 28 )
-        toolbar.setContentsMargins( 0, 0, 0, 0 )
-        return toolbar
+        self.__toolbar = QToolBar( self )
+        self.__toolbar.setOrientation( Qt.Vertical )
+        self.__toolbar.setMovable( False )
+        self.__toolbar.setAllowedAreas( Qt.RightToolBarArea )
+        self.__toolbar.setIconSize( QSize( 16, 16 ) )
+        self.__toolbar.setFixedWidth( 28 )
+        self.__toolbar.setContentsMargins( 0, 0, 0, 0 )
+
+        self.__infoIcon = QLabel()
+        self.__infoIcon.setPixmap( PixmapCache().getPixmap( 'cfunknown.png' ) )
+        self.__toolbar.addWidget( self.__infoIcon )
+        return self.__toolbar
 
     def __createGraphicsView( self ):
         """ Creates the graphics view """
@@ -100,28 +139,102 @@ class FlowUIWidget( QWidget ):
         self.view.setScene( self.scene )
         return self.view
 
-    def process( self, content ):
-        """ Parses the content and displays the results """
-
-        self.cf = getControlFlowFromMemory( content )
-        if len( self.cf.errors ) != 0:
-            print "No drawing due to parsing errors"
+    def __updateInfoIcon( self, state ):
+        " Updates the information icon "
+        if state == self.__currentIconState:
             return
 
-        if len( self.cf.warnings ) != 0:
-            self.logMessage( "Parser warnings: " )
-            for warn in self.cf.warnings:
-                print str( warn[0] ) + ": " + warn[1]
+        if state == self.STATE_OK_UTD:
+            self.__infoIcon.setPixmap( PixmapCache().getPixmap( 'cfokutd.png' ) )
+            self.__infoIcon.setToolTip( "Control flow is up to date" )
+            self.__currentIconState = self.STATE_OK_UTD
+        elif state == self.STATE_OK_CHN:
+            self.__infoIcon.setPixmap( PixmapCache().getPixmap( 'cfokchn.png' ) )
+            self.__infoIcon.setToolTip( "Control flow is not up to date; will be updated on idle" )
+            self.__currentIconState = self.STATE_OK_CHN
+        elif state == self.STATE_BROKEN_UTD:
+            self.__infoIcon.setPixmap( PixmapCache().getPixmap( 'cfbrokenutd.png' ) )
+            self.__infoIcon.setToolTip( "Control flow might be invalid due to invalid python code" )
+            self.__currentIconState = self.STATE_BROKEN_UTD
+        else:
+            # STATE_BROKEN_CHN
+            self.__infoIcon.setPixmap( PixmapCache().getPixmap( 'cfbrokenchn.png' ) )
+            self.__infoIcon.setToolTip( "Control flow might be invalid; will be updated on idle" )
+            self.__currentIconState = self.STATE_BROKEN_CHN
+        return
+
+    def process( self ):
+        """ Parses the content and displays the results """
+
+        if not self.__connected:
+            self.__connectEditorSignals()
+
+        content = self.__editor.text()
+        self.__cf = getControlFlowFromMemory( content )
+        if len( self.__cf.errors ) != 0:
+            self.__updateInfoIcon( self.STATE_BROKEN_UTD )
+            return
+
+        self.__updateInfoIcon( self.STATE_OK_UTD )
+
+#        if len( self.__cf.warnings ) != 0:
+#            self.logMessage( "Parser warnings: " )
+#            for warn in self.__cf.warnings:
+#                print str( warn[0] ) + ": " + warn[1]
 
         self.scene.clear()
         try:
             # Top level canvas has no adress and no parent canvas
             canvas = VirtualCanvas( self.cflowSettings, None, None, None )
-            canvas.layout( self.cf )
+            canvas.layout( self.__cf )
             width, height = canvas.render()
             self.scene.setSceneRect( 0, 0, width, height )
             canvas.draw( self.scene, 0, 0 )
         except Exception, exc:
             print "Exception:\n" + str( exc )
         return
+
+    def __onFileTypeChanged( self, fileName, uuid, newFileType ):
+        " Triggered when a buffer content type has changed "
+
+        if self.__parentWidget.getUUID() != uuid:
+            return
+
+        if newFileType not in [ Python3FileType, PythonFileType ]:
+            self.__disconnectEditorSignals()
+            self.__updateTimer.stop()
+            self.__cf = None
+            self.setVisible( False )
+            self.__currentIconState = self.STATE_UNKNOWN
+            return
+
+        # Update the bar and show it
+        self.setVisible( True )
+        self.process()
+        return
+
+    def __connectEditorSignals( self ):
+        " When it is a python file - connect to the editor signals "
+        if not self.__connected:
+            self.__editor.SCEN_CHANGE.connect( self.__onBufferChanged )
+            self.__connected = True
+        return
+
+    def __disconnectEditorSignals( self ):
+        " Disconnect the editor signals when the file is not a python one "
+        if self.__connected:
+            self.__editor.SCEN_CHANGE.disconnect( self.__onBufferChanged )
+            self.__connected = False
+        return
+
+    def __onBufferChanged( self ):
+        " Triggered to update status icon and to restart the timer "
+        self.__updateTimer.stop()
+        if len( self.__cf.errors ) == 0:
+            self.__updateInfoIcon( self.STATE_OK_CHN )
+        else:
+            self.__updateInfoIcon( self.STATE_BROKEN_CHN )
+        self.__updateTimer.start( IDLE_TIMEOUT )
+        return
+
 
