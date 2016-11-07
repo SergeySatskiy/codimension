92b948fd2995a767bea28519113e188141d603c3
SergeySatskiy <sergey.satskiy@gmail.com>
2016-11-07 17:39:05 -0500

    Porting to python 3

--- src/utils/briefmodinfocache.py
+++ src/utils/briefmodinfocache.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # codimension - graphics python two-way code editor and analyzer
-# Copyright (C) 2010  Sergey Satskiy <sergey.satskiy@gmail.com>
+# Copyright (C) 2010-2016  Sergey Satskiy <sergey.satskiy@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,13 +17,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# $Id$
-#
 
 
 """ codimension brief module info cache """
 
-import os, os.path
+from os.path import realpath, getmtime, exists
 from cdmbriefparser import getBriefModuleInfoFromFile
 
 
@@ -31,45 +29,46 @@ from cdmbriefparser import getBriefModuleInfoFromFile
 class BriefModuleInfoCache():
     """ Provides the module info cache """
 
-    def __init__( self ):
-        # abs file path -> [ modification time, mod info ]
+    def __init__(self):
+        # abs file path -> (modification time, mod info)
         self.__cache = {}
         return
 
-    def get( self, path ):
+    def get(self, path):
         """ Provides the required modinfo """
 
-        path = os.path.abspath( str( path ) )
+        path = realpath(path)
         try:
-            modInfo = self.__cache[ path ]
-            if not os.path.exists( path ):
-                del self.__cache[ path ]
-                raise Exception( "Cannot open " + path )
+            modInfo = self.__cache[path]
+            if not exists(path):
+                del self.__cache[path]
+                raise Exception("Cannot open " + path)
 
-            lastModTime = os.path.getmtime( path )
-            if lastModTime <= modInfo[ 0 ]:
-                return modInfo[ 1 ]
+            lastModTime = getmtime(path)
+            if lastModTime <= modInfo[0]:
+                return modInfo[1]
 
             # update the key
-            info = getBriefModuleInfoFromFile( path )
-            self.__cache[ path ] = [ lastModTime, info ]
+            info = getBriefModuleInfoFromFile(path)
+            self.__cache[path] = (lastModTime, info)
             return info
         except KeyError:
-            if not os.path.exists( path ):
-                raise Exception( "Cannot open " + path )
+            if not exists(path):
+                raise Exception("Cannot open " + path)
 
-            info = getBriefModuleInfoFromFile( path )
-            self.__cache[ path ] = [ os.path.getmtime( path ), info ]
+            info = getBriefModuleInfoFromFile(path)
+            self.__cache[path] = (getmtime(path), info)
             return info
 
-    def remove( self, path ):
+    def remove(self, path):
         " Removes one file from the map "
+        path = realpath(path)
         try:
-            del self.__cache[ path ]
+            del self.__cache[path]
         except KeyError:
             return
 
-    def clear( self ):
+    def clear(self):
         " Clears the cache "
         self.__cache = {}
         return
