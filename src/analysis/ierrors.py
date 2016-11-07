6a655eeb46479a355c7bce32e43907a92e8bafaa
SergeySatskiy <sergey.satskiy@gmail.com>
2016-11-07 16:20:10 -0500

    Make pylint happy

--- src/analysis/ierrors.py
+++ src/analysis/ierrors.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # codimension - graphics python two-way code editor and analyzer
-# Copyright (C) 2010-2012  Sergey Satskiy <sergey.satskiy@gmail.com>
+# Copyright (C) 2010-2016  Sergey Satskiy <sergey.satskiy@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,8 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# $Id$
-#
 
 " Interactive errors report using pyflakes "
 
@@ -26,41 +24,40 @@ from _ast import PyCF_ONLY_AST
 from pyflakes.checker import Checker
 
 
-def getFileErrors( sourceCode ):
+def getFileErrors(sourceCode):
     " Provides a list of warnings/errors for the given source code "
 
     sourceCode += '\n'
 
     # First, compile into an AST and handle syntax errors.
     try:
-        tree = compile( sourceCode, "<string>", "exec", PyCF_ONLY_AST )
-    except SyntaxError, value:
+        tree = compile(sourceCode, "<string>", "exec", PyCF_ONLY_AST)
+    except SyntaxError as value:
         # If there's an encoding problem with the file, the text is None.
         if value.text is None:
             return []
-        return [ ( value.args[0], value.lineno ) ]
-    except ( ValueError, TypeError ), value:
+        return [(value.args[0], value.lineno)]
+    except (ValueError, TypeError) as value:
         # ValueError may happened in case of invalid \x escape character
         # E.g. http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=674797
         # TypeError may happened in case of null characters in a file
         # E.g. http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=674796
-        msg = str( value )
+        msg = str(value)
         if msg == "":
-            return [ ( "Could not compile buffer: unknown error", -1 ) ]
-        return [ ( "Could not compile buffer: " + msg, -1 ) ]
+            return [("Could not compile buffer: unknown error", -1)]
+        return [("Could not compile buffer: " + msg, -1)]
 
     # Okay, it's syntactically valid.  Now check it.
-    w = Checker( tree, "<string>" )
+    check = Checker(tree, "<string>")
     results = []
     lines = sourceCode.splitlines()
-    for warning in w.messages:
-        if type( warning.lineno ) == int:
+    for warning in check.messages:
+        if isinstance(warning.lineno, int):
             lineno = warning.lineno
         else:
             # By some reasons I see ast NAME node here (pyflakes 0.7.3)
             lineno = warning.lineno.lineno
-        if 'analysis:ignore' not in lines[ lineno - 1 ]:
-            results.append( ( warning.message % warning.message_args,
-                              lineno ) )
-    results.sort( key = lambda x: x[ 1 ] )
+        if 'analysis:ignore' not in lines[lineno - 1]:
+            results.append((warning.message % warning.message_args, lineno))
+    results.sort(key=lambda x: x[1])
     return results
