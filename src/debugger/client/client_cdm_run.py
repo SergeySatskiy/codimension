4da66632f3de3c0837fb5bed63028cad637c38a1
SergeySatskiy <sergey.satskiy@gmail.com>
2017-01-01 16:50:40 -0500

    Cosmetics

--- src/debugger/client/client_cdm_run.py
+++ src/debugger/client/client_cdm_run.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # codimension - graphics python two-way code editor and analyzer
-# Copyright (C) 2014  Sergey Satskiy <sergey.satskiy@gmail.com>
+# Copyright (C) 2014-2016  Sergey Satskiy <sergey.satskiy@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,16 +17,19 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# $Id$
-#
 
-""" Wrapper to run a script with redirected IO """
+"""Wrapper to run a script with redirected IO"""
 
 
-import sys, socket, time, traceback, os, imp
-from outredir_cdm_dbg import OutStreamRedirector, MAX_TRIES
-from protocol_cdm_dbg import ( RequestContinue, EOT, ResponseExit,
-                               ResponseRaw, RequestExit, ResponseProcID )
+import sys
+import socket
+import time
+import traceback
+import os
+import imp
+from .outredir_cdm_dbg import OutStreamRedirector, MAX_TRIES
+from .protocol_cdm_dbg import (RequestContinue, EOT, ResponseExit,
+                               ResponseRaw, RequestExit, ResponseProcID)
 from errno import EAGAIN
 
 
@@ -35,100 +38,101 @@ WAIT_EXIT_COMMAND = 5
 RUN_WRAPPER = None
 
 
-def runClientRawInput( prompt = "", echo = 1 ):
-    " Replacement for the standard raw_input builtin "
-    if RUN_WRAPPER is None or RUN_WRAPPER.redirected() == False:
-        return runClientOrigRawInput( prompt )
-    return RUN_WRAPPER.raw_input( prompt, echo )
+def runClientRawInput(prompt="", echo=True):
+    """Replacement for the standard raw_input builtin"""
+    if RUN_WRAPPER is None or not RUN_WRAPPER.redirected():
+        return runClientOrigRawInput(prompt)
+    return RUN_WRAPPER.raw_input(prompt, echo)
+
 
 # Use our own raw_input().
 try:
-    runClientOrigRawInput = __builtins__.__dict__[ 'raw_input' ]
-    __builtins__.__dict__[ 'raw_input' ] = runClientRawInput
-except ( AttributeError, KeyError ):
+    runClientOrigRawInput = __builtins__.__dict__['raw_input']
+    __builtins__.__dict__['raw_input'] = runClientRawInput
+except (AttributeError, KeyError):
     import __main__
-    runClientOrigRawInput = __main__.__builtins__.__dict__[ 'raw_input' ]
-    __main__.__builtins__.__dict__[ 'raw_input' ] = runClientRawInput
+    runClientOrigRawInput = __main__.__builtins__.__dict__['raw_input']
+    __main__.__builtins__.__dict__['raw_input'] = runClientRawInput
 
 
-def runClientInput( prompt = "" ):
-    " Replacement for the standard input builtin "
-    if RUN_WRAPPER is None or RUN_WRAPPER.redirected() == False:
-        return runClientOrigInput( prompt )
-    return RUN_WRAPPER.input( prompt )
+def runClientInput(prompt=""):
+    """Replacement for the standard input builtin"""
+    if RUN_WRAPPER is None or not RUN_WRAPPER.redirected():
+        return runClientOrigInput(prompt)
+    return RUN_WRAPPER.input(prompt)
+
 
 # Use our own input().
 try:
-    runClientOrigInput = __builtins__.__dict__[ 'input' ]
-    __builtins__.__dict__[ 'input' ] = runClientInput
-except ( AttributeError, KeyError ):
+    runClientOrigInput = __builtins__.__dict__['input']
+    __builtins__.__dict__['input'] = runClientInput
+except (AttributeError, KeyError):
     import __main__
-    runClientOrigInput = __main__.__builtins__.__dict__[ 'input' ]
-    __main__.__builtins__.__dict__[ 'input' ] = runClientInput
-
+    runClientOrigInput = __main__.__builtins__.__dict__['input']
+    __main__.__builtins__.__dict__['input'] = runClientInput
 
 
 class RedirectedIORunWrapper():
-    " Wrapper to run a script with redirected IO "
 
-    def __init__( self ):
+    """Wrapper to run a script with redirected IO"""
+
+    def __init__(self):
         self.__socket = None
         self.__redirected = False
-        return
 
-    def redirected( self ):
-        " True if streams are redirected "
+    def redirected(self):
+        """True if streams are redirected"""
         return self.__redirected
 
-    def main( self ):
-        " Run wrapper driver "
+    def main(self):
+        "Run wrapper driver"
         if '--' not in sys.argv:
-            print >> sys.stderr, "Unexpected arguments"
+            print("Unexpected arguments", file=sys.stderr)
             return 1
 
         procid, host, port, wdir, args = self.parseArgs()
         if procid is None or host is None or port is None or wdir is None:
-            print >> sys.stderr, "Not enough arguments"
+            print("Not enough arguments", file=sys.stderr)
             return 1
 
-        remoteAddress = self.resolveHost( host )
-        self.connect( remoteAddress, port )
-        self.write( ResponseProcID + str( procid ) )
+        remoteAddress = self.resolveHost(host)
+        self.connect(remoteAddress, port)
+        self.write(ResponseProcID + str(procid))
 
         try:
             self.__waitContinue()
-        except Exception, exc:
-            print >> sys.stderr, str( exc )
+        except Exception as exc:
+            print(str(exc), file=sys.stderr)
             return 1
 
         # Setup redirections
         stdoutOld = sys.stdout
         stderrOld = sys.stderr
-        sys.stdout = OutStreamRedirector( self.__socket, True )
-        sys.stderr = OutStreamRedirector( self.__socket, False )
+        sys.stdout = OutStreamRedirector(self.__socket, True)
+        sys.stderr = OutStreamRedirector(self.__socket, False)
         self.__redirected = True
 
         # Run the script
         retCode = 0
         try:
-            self.__runScript( wdir, args )
-        except SystemExit, exc:
+            self.__runScript(wdir, args)
+        except SystemExit as exc:
             if exc.code is None:
                 retCode = 0
-            elif type( exc.code ) == int:
+            elif isinstance(exc.code, int):
                 retCode = exc.code
             else:
                 retCode = 1
-                print >> sys.stderr, str( exc.code )
-        except KeyboardInterrupt, exc:
+                print(str(exc.code), file=sys.stderr)
+        except KeyboardInterrupt as exc:
             retCode = 1
-            print >> sys.stderr, traceback.format_exc()
-        except Exception, exc:
+            print(traceback.format_exc(), file=sys.stderr)
+        except Exception as exc:
             retCode = 1
-            print >> sys.stderr, traceback.format_exc()
+            print(traceback.format_exc(), file=sys.stderr)
         except:
             retCode = 1
-            print >> sys.stderr, traceback.format_exc()
+            print(traceback.format_exc(), file=sys.stderr)
 
         sys.stderr = stderrOld
         sys.stdout = stdoutOld
@@ -136,110 +140,106 @@ class RedirectedIORunWrapper():
 
         # Send the return code back
         try:
-            self.write( ResponseExit + str( retCode ) )
-        except Exception, exc:
-            print >> sys.stderr, str( exc )
+            self.write(ResponseExit + str(retCode))
+        except Exception as exc:
+            print(str(exc), file=sys.stderr)
             self.close()
             return 1
 
         self.close()
         return 0
 
-    def connect( self, remoteAddress, port ):
-        " Establishes a connection with the IDE "
+    def connect(self, remoteAddress, port):
+        """Establishes a connection with the IDE"""
         if remoteAddress is None:                    # default: 127.0.0.1
-            self.__socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-            self.__socket.connect( ( '127.0.0.1', port ) )
+            self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.__socket.connect(('127.0.0.1', port))
         else:
             if "@@i" in remoteAddress:
-                remoteAddress, index = remoteAddress.split( "@@i" )
+                remoteAddress, index = remoteAddress.split("@@i")
             else:
                 index = 0
             if ":" in remoteAddress:                              # IPv6
-                sockaddr = socket.getaddrinfo( remoteAddress, port, 0, 0,
-                                               socket.SOL_TCP )[ 0 ][ -1 ]
-                self.__socket = socket.socket( socket.AF_INET6,
-                                               socket.SOCK_STREAM )
-                sockaddr = sockaddr[ : -1 ] + ( int( index ), )
-                self.__socket.connect( sockaddr )
+                sockaddr = socket.getaddrinfo(remoteAddress, port, 0, 0,
+                                              socket.SOL_TCP)[0][-1]
+                self.__socket = socket.socket(socket.AF_INET6,
+                                              socket.SOCK_STREAM)
+                sockaddr = sockaddr[:-1] + (int(index),)
+                self.__socket.connect(sockaddr)
             else:                                                   # IPv4
-                self.__socket = socket.socket( socket.AF_INET,
-                                               socket.SOCK_STREAM )
-                self.__socket.connect( ( remoteAddress, port ) )
-        return
+                self.__socket = socket.socket(socket.AF_INET,
+                                              socket.SOCK_STREAM)
+                self.__socket.connect((remoteAddress, port))
 
-    def __waitContinue( self ):
-        " Waits the 'continue' command "
+    def __waitContinue(self):
+        """Waits the 'continue' command"""
         startTime = time.time()
         while True:
-            time.sleep( 0.01 )
+            time.sleep(0.01)
 
             # Read from the socket
             try:
-                data = self.__socket.recv( 1024, socket.MSG_DONTWAIT )
-            except socket.error, exc:
-                if exc[ 0 ] != EAGAIN:
+                data = self.__socket.recv(1024, socket.MSG_DONTWAIT)
+            except socket.error as exc:
+                if exc[0] != EAGAIN:
                     raise
                 data = None
             if not data:
                 if time.time() - startTime > WAIT_CONTINUE_TIMEOUT:
-                    raise Exception( "Continue command timeout" )
+                    raise Exception("Continue command timeout")
             else:
                 if data == RequestContinue + EOT:
                     break
-                raise Exception( "Unexpected command from IDE: " + data )
-        return
+                raise Exception("Unexpected command from IDE: " + data)
 
-    def __waitExit( self ):
-        " Waits for the 'exit' command "
+    def __waitExit(self):
+        """Waits for the 'exit' command"""
         startTime = time.time()
         while True:
-            time.sleep( 0.01 )
+            time.sleep(0.01)
 
             # Read from the socket
             try:
-                data = self.__socket.recv( 1024, socket.MSG_DONTWAIT )
-            except socket.error, exc:
-                if exc[ 0 ] != EAGAIN:
+                data = self.__socket.recv(1024, socket.MSG_DONTWAIT)
+            except socket.error as exc:
+                if exc[0] != EAGAIN:
                     raise
                 data = None
             if data is None:
                 if time.time() - startTime > WAIT_EXIT_COMMAND:
-                    raise Exception( "Exit command timeout" )
+                    raise Exception("Exit command timeout")
             else:
                 if data == RequestExit + EOT:
                     break
-                raise Exception( "Unexpected command from IDE: " + data )
-        return
+                raise Exception("Unexpected command from IDE: " + data)
 
-    def write( self, data ):
-        " Writes into the socket "
+    def write(self, data):
+        """Writes into the socket"""
         tries = MAX_TRIES
         while tries > 0:
             try:
                 if self.__socket:
-                    self.__socket.sendall( data + EOT )
+                    self.__socket.sendall(data + EOT)
                 return
             except socket.error:
                 tries -= 1
                 continue
-        raise socket.error( "Too many attempts to send data" )
+        raise socket.error("Too many attempts to send data")
 
-    def __runScript( self, workingDir, arguments ):
-        " Runs the python script "
-
+    def __runScript(self, workingDir, arguments):
+        """Runs the python script"""
         try:
             # In Py 2.x, the builtins were in __builtin__
-            builtins = sys.modules[ '__builtin__' ]
+            builtins = sys.modules['__builtin__']
         except KeyError:
             # In Py 3.x, they're in builtins
-            builtins = sys.modules[ 'builtins' ]
+            builtins = sys.modules['builtins']
 
-        fileName = arguments[ 0 ]
+        fileName = arguments[0]
 
-        oldMainMod = sys.modules[ '__main__' ]
-        mainMod = imp.new_module( '__main__' )
-        sys.modules[ '__main__' ] = mainMod
+        oldMainMod = sys.modules['__main__']
+        mainMod = imp.new_module('__main__')
+        sys.modules['__main__'] = mainMod
         mainMod.__file__ = fileName
         mainMod.__builtins__ = builtins
 
@@ -249,10 +249,10 @@ class RedirectedIORunWrapper():
 
         # Without this imports of what is located at the script directory do
         # not work
-        sys.path.insert( 0, workingDir )
+        sys.path.insert(0, workingDir)
 
-        os.chdir( workingDir )
-        f = open( fileName, "rU" )
+        os.chdir(workingDir)
+        f = open(fileName, "rU")
         source = f.read()
         f.close()
 
@@ -261,18 +261,17 @@ class RedirectedIORunWrapper():
         if not source or source[-1] != '\n':
             source += '\n'
 
-        code = compile( source, fileName, "exec" )
-        exec( code, mainMod.__dict__ )
+        code = compile(source, fileName, "exec")
+        exec(code, mainMod.__dict__)
 
         # Restore the old __main__
-        sys.modules[ '__main__' ] = oldMainMod
+        sys.modules['__main__'] = oldMainMod
 
         # Restore the old argv and path
         sys.argv = oldArgv
-        return
 
-    def close( self ):
-        " Closes the connection if so "
+    def close(self):
+        """Closes the connection if so"""
         try:
             if self.__socket:
                 # Wait exit needed because otherwise IDE may get socket
@@ -283,90 +282,88 @@ class RedirectedIORunWrapper():
                 self.__socket.close()
         except:
             pass
-        return
 
-    def raw_input( self, prompt, echo ):
-        " Implements raw_input() using the redirected input "
+    def raw_input(self, prompt, echo):
+        """Implements raw_input() using the redirected input"""
         # self.__flushSocketBuffer()
-        self.write( "%s%s" % ( ResponseRaw, unicode( ( prompt, echo ) ) ) )
+        self.write("%s%s" % (ResponseRaw, (prompt, echo)))
         return self.__waitInput()
 
-    def __flushSocketBuffer( self ):
-        " Debug purpose function which empties the socket buffer "
+    def __flushSocketBuffer(self):
+        """Debug purpose function which empties the socket buffer"""
         try:
-            data = self.__socket.recv( 1024, socket.MSG_DONTWAIT )
+            data = self.__socket.recv(1024, socket.MSG_DONTWAIT)
             if data is not None:
-                if len( data ) > 0:
-                    f = open( "clientsocket.txt", "a" )
-                    f.write( "UNEXPECTED data in socket: " + repr( data ) + "\n" )
+                if len(data) > 0:
+                    f = open("clientsocket.txt", "a")
+                    f.write("UNEXPECTED data in socket: " + repr(data) + "\n")
                     f.close()
         except:
             pass
-        return
 
-    def input( self, prompt ):
-        " Implement input() using the redirected input "
-        return self.raw_input( prompt, 1 )
+    def input(self, prompt):
+        """Implement input() using the redirected input"""
+        return self.raw_input(prompt, 1)
 
-    def __waitInput( self ):
-        " Waits for the user input "
+    def __waitInput(self):
+        """Waits for the user input"""
         buf = u""
         while True:
             try:
-                data = self.__socket.recv( 4096 )
+                data = self.__socket.recv(4096)
                 if data is not None:
-                    pos = data.find( '\n' )
+                    pos = data.find('\n')
                     if pos != -1:
-                        buf += data[ 0 : pos ].decode( 'utf8' )
+                        buf += data[0:pos].decode('utf8')
                         break
 
-                    buf += data.decode( 'utf8' )
-            except Exception, exc:
-                f = open( "clientsocket.txt", "a" )
-                f.write( "Wait input exception: " + str( exc ) + "\n" )
+                    buf += data.decode('utf8')
+            except Exception as exc:
+                f = open("clientsocket.txt", "a")
+                f.write("Wait input exception: " + str(exc) + "\n")
                 f.close()
         return buf
 
     @staticmethod
-    def resolveHost( host ):
-        " Resolves a hostname to an IP address "
+    def resolveHost(host):
+        """Resolves a hostname to an IP address"""
         try:
-            host, version = host.split( "@@" )
+            host, version = host.split("@@")
             family = socket.AF_INET6
         except ValueError:
             # version = 'v4'
             family = socket.AF_INET
-        return socket.getaddrinfo( host, None, family,
-                                   socket.SOCK_STREAM )[ 0 ][ 4 ][ 0 ]
+        return socket.getaddrinfo(host, None, family,
+                                  socket.SOCK_STREAM)[0][4][0]
 
     @staticmethod
     def parseArgs():
-        " Parses the arguments "
+        """Parses the arguments"""
         host = None
         port = None
         wdir = None
         procid = None
-        args = sys.argv[ 1 : ]
+        args = sys.argv[1:]
 
-        while args[ 0 ]:
-            if args[ 0 ] in [ '-h', '--host' ]:
-                host = args[ 1 ]
-                del args[ 0 ]
-                del args[ 0 ]
-            elif args[ 0 ] in [ '-p', '--port' ]:
-                port = int( args[ 1 ] )
-                del args[ 0 ]
-                del args[ 0 ]
-            elif args[ 0 ] in [ '-w', '--workdir' ]:
-                wdir = args[ 1 ]
-                del args[ 0 ]
-                del args[ 0 ]
-            elif args[ 0 ] in [ '-i', '--procid' ]:
-                procid = int( args[ 1 ] )
-                del args[ 0 ]
-                del args[ 0 ]
-            elif args[ 0 ] == '--':
-                del args[ 0 ]
+        while args[0]:
+            if args[0] in ['-h', '--host']:
+                host = args[1]
+                del args[0]
+                del args[0]
+            elif args[0] in ['-p', '--port']:
+                port = int(args[1])
+                del args[0]
+                del args[0]
+            elif args[0] in ['-w', '--workdir']:
+                wdir = args[1]
+                del args[0]
+                del args[0]
+            elif args[0] in ['-i', '--procid']:
+                procid = int(args[1])
+                del args[0]
+                del args[0]
+            elif args[0] == '--':
+                del args[0]
                 break
 
         return procid, host, port, wdir, args
@@ -374,5 +371,4 @@ class RedirectedIORunWrapper():
 
 if __name__ == "__main__":
     RUN_WRAPPER = RedirectedIORunWrapper()
-    sys.exit( RUN_WRAPPER.main() )
-
+    sys.exit(RUN_WRAPPER.main())
